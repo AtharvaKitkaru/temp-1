28cf14a90fbdef23d5ec79ac285c90c7c8afb755
nishavak <nishavak.n@somaiya.edu>
2020-06-20 11:38:57 +0530

    added shade to topbar

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -11,7 +11,8 @@ import TopBarProgress from "react-topbar-progress-indicator";
 TopBarProgress.config({
   barColors: {
     "0": "rgb(183, 32, 46)",
-    "0.5": "white",
+    "0.5": "#ffcccb",
+    "0.75": "white",
     "1.0": "white",
   },
   shadowBlur: 5,
