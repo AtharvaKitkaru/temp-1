bf417b320f6fadb0a0ed531f8f5db84a9a0b7bcb
nishavak <nishavak.n@somaiya.edu>
2020-06-21 13:51:10 +0530

    added group registration

--- src/components/forms/groupRegistration/GroupRegistration.js
+++ src/components/forms/groupRegistration/GroupRegistration.js
@@ -1,12 +1,84 @@
 import React, { Component } from "react";
+import { Modal } from "react-bootstrap";
 import "./GroupRegistration.scss";
 
 class GroupRegistration extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: true,
+    };
+  }
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
   render() {
     return (
-      <div id="group-registration">
-        <p>Group Registration form</p>
-      </div>
+      <Modal
+        backdrop
+        scrollable
+        centered
+        show={this.state.show}
+        onHide={() => this.setState({ show: false })}
+      >
+        <Modal.Header closeButton className="">
+          <div className="h4 col-11 text-center">Group Registration</div>
+        </Modal.Header>
+        <Modal.Body>
+          <form
+            onSubmit={this.handleSubmit}
+            id="group-registration"
+            className="col-10 mx-auto"
+          >
+            <div className="form-group">
+              <label htmlFor="leader-roll-number">Leader Roll number</label>
+              <input
+                type="number"
+                className="form-control"
+                id="leader-roll-number"
+                name="leader-roll-number"
+                placeholder="Enter leader's roll number"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="leader-name">Leader name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="leader-name"
+                name="leader-name"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="leader-email">Leader email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="leader-email"
+                name="leader-email"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <button
+              style={{
+                backgroundColor: "rgb(183, 32, 46)",
+                color: "white",
+              }}
+              className="btn shadow-sm"
+            >
+              Submit
+            </button>
+          </form>
+        </Modal.Body>
+      </Modal>
     );
   }
 }
