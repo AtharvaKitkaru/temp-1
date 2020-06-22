b89b9eaf3c51eee1911f00ca1e86b5acc8d8b08f
AtharvaKitkaru <54904845+AtharvaKitkaru@users.noreply.github.com>
2020-06-22 22:51:30 +0530

    initial commit

--- src/components/assignmentDetails/AssignmentDetails.js
+++ src/components/assignmentDetails/AssignmentDetails.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import "./AssignmentDetails.scss";
+import CommentSection from './CommentSection';
+import DetailsSection from './DetailsSection';
+import TaskSection from './TaskSection';
 
 class AssignmentDetails extends Component {
   render() {
     return (
-      <div id="assignment-details">
-        <p>AssignmentDetails</p>
+      <div className= "container">
+        <div className="row">
+          <div className = "col-12 col-md-3 mx-auto">
+            <TaskSection />
+          </div>
+          <div className = "col-12 col-md-8 mx-auto">
+            <DetailsSection />
+          </div>        
+        </div>
+        <div className = "col-12 text-center">
+          <CommentSection />
+        </div>
       </div>
     );
   }
---
+++ src/components/assignmentDetails/CommentSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class CommentSection extends React.Component{
+  render(){
+    return(
+      <div className="container p-1 shadow-sm">
+          <p className="" >Comment Section</p>
+      </div>
+    );
+  }
+}
+export default CommentSection;
