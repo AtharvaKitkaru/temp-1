dcd999e91a09dc84a299446b023314dfa8971158
nishavak <nishavak.n@somaiya.edu>
2020-06-24 11:18:17 +0530

    added comment section

--- src/components/assignmentDetails/CommentSection.js
+++ src/components/assignmentDetails/CommentSection.js
@@ -1,10 +1,35 @@
 import React from "react";
-
+import { Form, Button } from "react-bootstrap";
+import "./CommentSection.css";
 class CommentSection extends React.Component {
   render() {
     return (
-      <div className="container p-1 shadow-sm">
-        <p className="">Comment Section</p>
+      <div className="container p-4 shadow-sm">
+        {/* <Form>
+            <Form.Group>
+                <Form.Control className = "input-field w-25 border-light shadow-sm" type="text" placeholder="Add your comment here ..." />
+
+                <Button className = "secondary w-25"  size = "sm" type = "reset">Cancel</Button>
+                <Button className = "primary w-25" size = "sm" type = "submit">Comment</Button>
+                
+              </Form.Group>
+          </Form> */}
+
+        <form>
+          <div className="row">
+            <div className="form-group col-12">
+              <input
+                type="text"
+                className="form-control shadow-sm"
+                placeholder="Add your comment here ..."
+              />
+            </div>
+          </div>
+
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
