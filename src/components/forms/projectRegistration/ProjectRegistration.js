677156d5591cb7c46d7cd5a5d512a0a09058762b
Jill Shah <56551133+Jill-S@users.noreply.github.com>
2020-06-27 00:57:11 +0530

    initial commit

--- src/components/forms/projectRegistration/ProjectRegistration.js
+++ src/components/forms/projectRegistration/ProjectRegistration.js
@@ -1,12 +1,201 @@
 import React, { Component } from "react";
 import "./ProjectRegistration.scss";
+import { Form, FormGroup, Label, Input } from "reactstrap";
+import { Button } from "reactstrap";
 
 class ProjectRegistration extends Component {
+  constructor() {
+    super();
+    this.state = JSON.parse(localStorage.getItem("projectRegistration")) || {};
+  }
+
+  handleChange = e => {
+    console.log(e.target.value);
+
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    localStorage.setItem("projectRegistration", JSON.stringify(this.state));
+    this.setState({ successfulSubmission: true });
+  };
+  handlePrevClick = () => {
+    localStorage.setItem("projectRegistration", JSON.stringify(this.state));
+    this.setState({ prevPagePermit: true });
+  };
   render() {
     return (
-      <div id="project-registration">
-        <p>ProjectRegistration</p>
+      //<div>
+      //<div style={{ margin: "2em" }} />
+      <div className="container shadow">
+        <div id="project-registration-link">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontWeight: "bold",
+              fontSize: 25
+            }}
+          >
+            <p>
+              <u>Project Registration</u>
+            </p>
+            {/*<div style={{}} />*/}
+          </div>
+          <Form
+            className="project-registration mx-auto my-md-1 mt-1 col-12"
+            onSubmit={this.handleSubmit}
+          >
+            <div className="row">
+              <FormGroup className="col-md-12 col-12">
+                <Label for="projName">
+                  Project Name<span style={{ color: "red" }}>*</span>
+                </Label>
+                <Input
+                  type="text"
+                  name="projName"
+                  id="projName"
+                  placeholder="Enter Project name"
+                  onChange={this.handleChange}
+                  value={this.state.projName}
+                />
+              </FormGroup>
+            </div>
+            <div className="row">
+              <FormGroup className="col-md-6 col-12">
+                <Label for="projType">
+                  Select Type of Project<span style={{ color: "red" }}>*</span>
+                </Label>
+                <Input
+                  type="select"
+                  name="projType"
+                  id="projType"
+                  onChange={this.handleChange}
+                  value={this.state.projType}
+                >
+                  <option>--Select--</option>
+                  <option>Internal</option>
+                  <option>External</option>
+                  <option>Inter-Disciplinary</option>
+                </Input>
+              </FormGroup>
+              <FormGroup className="col-md-6 col-12">
+                <Label for="projDomain">
+                  Select Domain of Project
+                  <span style={{ color: "red" }}>*</span>
+                </Label>
+                <Input
+                  type="select"
+                  name="projDomain"
+                  id="projDomain"
+                  onChange={this.handleChange}
+                  value={this.state.projDomain}
+                >
+                  <option>--Select--</option>
+                  <option>
+                    Cyber Security (Forensics, Blockchain Technology, Biometrics
+                    Security, Cryptographic Teachniques)
+                  </option>
+                  <option>Image Processing (Computer Vision)</option>
+                  <option>
+                    Artificial Intelligence (Machine Learning, Natural Language
+                    Processing, Robotics)
+                  </option>
+                  <option>Computer Networking</option>
+                  <option>Big Data Processing</option>
+                  <option>
+                    Embedded and Hardware Integrated Applications (IOT)
+                  </option>
+                  <option>
+                    Augmented and Virtual Reality (Game Programming)
+                  </option>
+                  <option>GIS</option>
+                  <option>Cloud Computing (High Performance Computing)</option>
+                  <option>
+                    System Programming (Compiler Constaruction, OS, Device
+                    Drivers)
+                  </option>
+                  <option>Quantum Computing</option>
+                  <option>Software Test Automation</option>
+                </Input>
+              </FormGroup>
+            </div>
+            <div className="row">
+              <FormGroup className="col-md-12 col-12">
+                <Label for="projDescription">
+                  Enter the Project Description
+                  <span style={{ color: "red" }}>*</span>
+                </Label>
+                <Input
+                  type="textarea"
+                  name="projDescription"
+                  rows="7"
+                  id="projDescription"
+                  placeholder="Description"
+                  onChange={this.handleChange}
+                  value={this.state.projDescription}
+                />
+              </FormGroup>
+            </div>
+            <div
+              className="row"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <FormGroup className="col-12 col-md-12">
+                <Label for="exampleFile">
+                  <Button
+                    className="btn"
+                    style={{
+                      backgroundColor: "rgb(183, 32, 46)",
+                      color: "white",
+                      outline: "none",
+                      boxShadow: "none"
+                    }}
+                  >
+                    {" "}
+                    Attach documents (if any)
+                  </Button>
+                </Label>
+                <Input
+                  type="file"
+                  name="file"
+                  id="exampleFile"
+                  className="d-none"
+                />
+              </FormGroup>
+            </div>
+            <br />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Button
+                className="btn"
+                style={{
+                  backgroundColor: "rgb(183, 32, 46)",
+                  color: "white",
+                  outline: "none",
+                  boxShadow: "none"
+                }}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </div>
       </div>
+      //</div>
     );
   }
 }
